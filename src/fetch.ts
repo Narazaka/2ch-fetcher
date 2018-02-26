469611dd3133a2534e94081ec2b0d716c97a1f8d
Narazaka <info@narazaka.net>
2018-02-26 16:44:17 +0900

    error handle

--- src/fetch.ts
+++ src/fetch.ts
@@ -6,7 +6,14 @@ const chardet = require("chardet");
 export const requestOptions: request.RequestPromiseOptions = {};
 
 export async function fetch(url: string) {
-    const responseBuffer = await request(url, {...requestOptions, encoding: null});
+    let responseBuffer;
+    try {
+        responseBuffer = await request(url, {...requestOptions, encoding: null});
+    } catch (error) {
+        const errorEncoding = chardet.detect(error.error);
+        const errorResponse = iconv.decode(error.error, errorEncoding);
+        throw new Error(`${error.message} ${errorResponse}`);
+    }
     const encoding = chardet.detect(responseBuffer);
     const response = iconv.decode(responseBuffer, encoding);
 
