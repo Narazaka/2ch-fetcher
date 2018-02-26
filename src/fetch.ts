e3e026e42b1b478e9c5d6cbe51778297d0977063
Narazaka <info@narazaka.net>
2018-02-26 16:16:35 +0900

    request options

--- src/fetch.ts
+++ src/fetch.ts
@@ -3,8 +3,10 @@ import * as request from "request-promise-native";
 // tslint:disable-next-line no-var-requires no-require-imports
 const chardet = require("chardet");
 
+export const requestOptions = {};
+
 export async function fetch(url: string) {
-    const responseBuffer = await request(url, {encoding: null});
+    const responseBuffer = await request(url, {...requestOptions, encoding: null});
     const encoding = chardet.detect(responseBuffer);
     const response = iconv.decode(responseBuffer, encoding);
 
